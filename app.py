8657c7fd51dd9d9fbdf830c083a477f2df22522e
ashinwz <297787884@qq.com>
2021-06-26 17:08:41 +0800

    add flask-bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, login_required, login_user
+from werkzeug.urls import url_parse
+from flask import url_for, request
+import forms
+import User
 
 app = Flask(__name__)
+app.secret_key = 's3cr3t'
+bootstrap = Bootstrap()
+bootstrap.init_app(app)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
 
-@app.route('/')
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.User()
+    return user
+
+@app.route('/home')
+def home():
+    return "hello"
+
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html')
+    if request.method == 'GET':
+        form = forms.LoginForm()
+    if request.method == 'POST':
+        form = forms.LoginForm()
+        print(form.submit.data)
+        print (form.validate_on_submit())
+        if form.validate_on_submit():
+            # user = User.User()
+            # login_user(user)
+            if form.submit.data == True:
+                return redirect('http://www.baidu.com')
+
+    return render_template('index.html',form=form)
 
 @app.route('/register')
 def register():
